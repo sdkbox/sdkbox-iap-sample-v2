27d651d04120da332dfc36c7592a27c7b46a04f8
Nite Luo <darkdukey@gmail.com>
2015-08-04 15:06:24 -0700

    Finish integration

--- projects/IapTestCpp/Classes/HelloWorldScene.cpp
+++ projects/IapTestCpp/Classes/HelloWorldScene.cpp
@@ -1,6 +1,9 @@
 #include "HelloWorldScene.h"
 
 USING_NS_CC;
+using namespace sdkbox;
+
+template <typename T> std::string tostr(const T& t) { std::ostringstream os; os<<t; return os.str(); }
 
 CCScene* HelloWorld::scene()
 {
@@ -9,10 +12,10 @@ CCScene* HelloWorld::scene()
     
     // 'layer' is an autorelease object
     HelloWorld *layer = HelloWorld::create();
-
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
@@ -27,64 +30,147 @@ bool HelloWorld::init()
         return false;
     }
     
-    CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
-    CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
-
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(HelloWorld::menuCloseCallback));
+    int winWidth = CCDirector::sharedDirector()->getWinSize().width;
     
-	pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/2 ,
-                                origin.y + pCloseItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition(CCPointZero);
-    this->addChild(pMenu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
+    IAP::init();
+    IAP::setDebug(true);
+    IAP::setListener(this);
     
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
+    CCMenuItemFont::setFontSize(48);
     
-    // position the label on the center of the screen
-    pLabel->setPosition(ccp(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - pLabel->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(pLabel, 1);
-
-    // add "HelloWorld" splash screen"
-    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    pSprite->setPosition(ccp(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(pSprite, 0);
+    _coinCount = 0;
+    
+    _txtCoin = CCLabelTTF::create("0", "Arial", 32);
+    _txtCoin->setAnchorPoint(cocos2d::CCPoint(0, 0));
+    _txtCoin->setPosition(cocos2d::CCPoint(800, 500));
+    addChild(_txtCoin);
+    
+    CCMenuItemFont *item = CCMenuItemFont::create("Load", this, menu_selector(HelloWorld::onRequestIAP));
+    item->setPosition(0, 0);
+    CCMenuItemFont *item2 = CCMenuItemFont::create("Restore", this, menu_selector(HelloWorld::onRestoreIAP));
+    item->setPosition(0, 100);
+    
+    CCMenu *menu = CCMenu::create(item, item2, NULL);
+    menu->setAnchorPoint(cocos2d::CCPoint(0, 0));
+    menu->setPosition(cocos2d::CCPoint(200, 400));
+    addChild(menu);
+    
+    _iapMenu = CCMenu::create();
+    _iapMenu->setPosition(winWidth/2, 200);
+    addChild(_iapMenu);
+    
+    //Close menu
+    CCMenuItemFont* close_menu_item = CCMenuItemFont::create("Close", this, menu_selector(HelloWorld::menuCloseCallback));
+    CCMenu *close_menu = CCMenu::create(close_menu_item, NULL);
+    close_menu->setAnchorPoint(cocos2d::CCPoint(0, 0));
+    close_menu->setPosition(cocos2d::CCPoint(800, 100));
+    addChild(close_menu);
+    
     
     return true;
 }
 
+void HelloWorld::onShowAds(CCObject* sender)
+{
+    CCLOG("Show Ads");
+}
+
+void HelloWorld::onRequestIAP(CCObject* sender)
+{
+    IAP::refresh();
+}
+
+void HelloWorld::onRestoreIAP(CCObject* sender)
+{
+    IAP::restore();
+}
+
+void HelloWorld::onIAP(CCObject* sender)
+{
+    CCNode* btn = static_cast<CCNode*>(sender);
+    Product* p = (Product*)btn->getUserData();
+    
+    CCLOG("Start IAP %s", p->name.c_str());
+    IAP::purchase(p->name);
+}
 
 void HelloWorld::menuCloseCallback(CCObject* pSender)
 {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
-	CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
 #else
     CCDirector::sharedDirector()->end();
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
 #endif
+}
+
+void HelloWorld::onInitialized(bool success)
+{
+    CCLOG("IAP initialized");
+}
+
+void HelloWorld::onSuccess(const Product &p)
+{
+    if (p.id == "com.cocos2dx.plugintest3") {
+        _coinCount += 1000;
+        _txtCoin->setString(tostr(_coinCount).c_str());
+    }
+    else if (p.id == "com.cocos2dx.plugintest2") {
+        _coinCount += 5000;
+        _txtCoin->setString(tostr(_coinCount).c_str());
+    }
+    else if (p.id == "com.cocos2dx.non1") {
+        //Remove ads
+    }
+    
+    CCLOG("Purchase Success: %s", p.id.c_str());
+}
+
+void HelloWorld::onFailure(const Product &p, const std::string &msg)
+{
+    CCLOG("Purchase Failed: %s", msg.c_str());
+}
+
+void HelloWorld::onCanceled(const Product &p)
+{
+    CCLOG("Purchase Canceled: %s", p.id.c_str());
+}
+
+void HelloWorld::onRestored(const Product& p)
+{
+    CCLOG("Purchase Restored: %s", p.name.c_str());
+}
+
+void HelloWorld::updateIAP(const std::vector<sdkbox::Product>& products)
+{
+    //
+    _iapMenu->removeAllChildren();
+    _products = products;
+    
+    int posX = 0;
+    int posY = 0;
+    
+    for (int i=0; i < _products.size(); i++)
+    {
+        CCLOG("iap: %s", _products[i].name.c_str());
+        
+        CCMenuItemFont* btn = CCMenuItemFont::create(_products[i].name.c_str(), this, menu_selector(HelloWorld::onIAP) );
+        btn->setColor(ccWHITE);
+        btn->setUserData(&_products[i]);
+        btn->setPosition(CCPoint(posX, posY));
+        _iapMenu->addChild(btn);
+        posY += 50;
+    }
+}
+
+void HelloWorld::onProductRequestSuccess(const std::vector<Product> &products)
+{
+    updateIAP(products);
+}
+
+void HelloWorld::onProductRequestFailure(const std::string &msg)
+{
+    CCLOG("Fail to load products");
 }
--- projects/IapTestCpp/Classes/HelloWorldScene.h
+++ projects/IapTestCpp/Classes/HelloWorldScene.h
@@ -2,21 +2,48 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "PluginIAP/PluginIAP.h"
 
-class HelloWorld : public cocos2d::CCLayer
+class HelloWorld : public cocos2d::CCLayer, public sdkbox::IAPListener
 {
 public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
-
+    virtual bool init();
+    
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::CCScene* scene();
     
     // a selector callback
     void menuCloseCallback(CCObject* pSender);
+    void onShowAds(CCObject* sender);
+    void onRequestIAP(CCObject* sender);
+    void onRestoreIAP(CCObject* sender);
+    void onIAP(CCObject* sender);
     
     // implement the "static node()" method manually
     CREATE_FUNC(HelloWorld);
+    
+private:
+    void updateIAP(const std::vector<sdkbox::Product>& products);
+    
+    virtual void onInitialized(bool success);
+    
+    virtual void onSuccess(sdkbox::Product const& p);
+    
+    virtual void onFailure(sdkbox::Product const& p, const std::string &msg);
+    
+    virtual void onCanceled(sdkbox::Product const& p);
+    
+    virtual void onRestored(sdkbox::Product const& p);
+    
+    virtual void onProductRequestSuccess(std::vector<sdkbox::Product> const &products);
+    
+    virtual void onProductRequestFailure(const std::string &msg);
+    
+    cocos2d::CCMenu* _iapMenu;
+    std::vector<sdkbox::Product> _products;
+    cocos2d::CCLabelTTF* _txtCoin;
+    int _coinCount;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
