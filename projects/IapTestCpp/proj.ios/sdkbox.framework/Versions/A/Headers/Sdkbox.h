8ee3c3495ecb9ac5a2dd6047c8759cd2ac763f73
wanghuan <wanghuan1115@live.cn>
2015-11-16 16:42:36 +0800

    sdkbox update for cpp

--- projects/IapTestCpp/proj.ios/sdkbox.framework/Versions/A/Headers/Sdkbox.h
+++ projects/IapTestCpp/proj.ios/sdkbox.framework/Versions/A/Headers/Sdkbox.h
@@ -7,10 +7,13 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
+#define SDKBOX_VERSION_STR  "sdkbox V1.4.1.7"
+
 namespace sdkbox {
 
     void init( const char* application_token, const char* application_key );
     void init( const char* application_token, const char* application_key, bool debug );
+    void setProjectType(const char* project_type);
 }
 
 #endif//_CC_SDKBOX_H_
